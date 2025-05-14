0ba1e9faa38ec4c26da96a40c3e06bc03d5c61ab
JannatulFerdous18 <jannatul.uiu@gmail.com>
2025-05-14 16:43:26 +0200

    Update Supabase credential

--- client/src/pages/SignInSide/SignInSide.tsx
+++ client/src/pages/SignInSide/SignInSide.tsx
@@ -53,88 +53,71 @@ const SignInSide: React.FC = () => {
 
   return (
     <PageLayout>
-      <MDBContainer className="my-5 gradient-form">
-        <MDBRow>
-          <MDBCol col="6" className="mb-5">
-            <div className="d-flex flex-column ms-5">
-              <div className="text-center">
-                <img
-                  src={wamflowLogo}
-                  alt="Wamflow Logo"
-                  style={{ width: "100px" }}
-                />
-                <h4 className="mt-1 mb-5 pb-1">Bias Management System</h4>
-              </div>
-              {error && (
-                <div
-                  className="text-danger text-center mb-3"
-                  style={{ fontWeight: "bold" }}
-                >
-                  {error}
-                </div>
-              )}
-              <MDBInput
-                wrapperClass="mb-4"
-                label="Username"
-                id="form1"
-                type="text"
-                value={user_name}
-                onChange={(e) => setuser_name(e.target.value)}
+      <MDBContainer className="gradient-form">
+        <div className="login-box d-flex flex-row">
+          <MDBCol md="6" className="login-left">
+            <div className="text-center mb-4">
+              <img
+                src={wamflowLogo}
+                alt="Wamflow Logo"
+                style={{ width: "100px" }}
               />
+              <h4 className="mt-3">Bias Management System</h4>
+            </div>
 
-              <MDBInput
-                wrapperClass="mb-4"
-                label="Password"
-                id="form2"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
+            {error && (
+              <div className="text-danger text-center mb-3">{error}</div>
+            )}
 
-              <div className="text-center pt-1 mb-5 pb-1">
-                <MDBBtn
-                  className="mb-4 w-100 gradient-custom-2"
-                  onClick={handleLogin}
-                >
-                  Sign in
-                </MDBBtn>
-              </div>
+            <MDBInput
+              wrapperClass="mb-4"
+              label="Username"
+              type="text"
+              value={user_name}
+              onChange={(e) => setuser_name(e.target.value)}
+            />
+            <MDBInput
+              wrapperClass="mb-4"
+              label="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
-              <div className="d-flex flex-row align-items-center justify-content-center pb-4 mb-4">
-                <p className="mb-0">Don't have an account?</p>
-                <MDBBtn
-                  outline
-                  className="mx-2"
-                  color="info"
-                  onClick={() => navigate("/register")}
-                >
-                  Register
-                </MDBBtn>
-              </div>
+            <div className="text-center">
+              <MDBBtn className="mb-4 w-100" onClick={handleLogin}>
+                Sign in
+              </MDBBtn>
+            </div>
+
+            <div className="text-center">
+              <p>Don't have an account?</p>
+              <MDBBtn
+                outline
+                color="info"
+                onClick={() => navigate("/register")}
+              >
+                Register
+              </MDBBtn>
             </div>
           </MDBCol>
 
-          <MDBCol col="6" className="mb-5">
-            <div
-              className="d-flex flex-column justify-content-center h-100 mb-4"
-              style={{ background: "#54B4D3" }}
-            >
-              <div className="text-black px-3 py-4 p-md-5 mx-md-4">
-                <h4 className="mb-4">
-                  Revolutionize the way you design and implement web-based
-                  applications
-                </h4>
-                <p className="small mb-0">
-                  Graphical notation that provides a UML-like notation that is
-                  specifically tailored to the needs of inter-organizational
-                  web-based applications. Empower your team, visualize, design,
-                  and collaborate effortlessly, bridging the gap between concept
-                  and implementation.
-                </p>
-              </div>
+          <MDBCol md="6" className="login-right">
+            <div>
+              <h4 className="mb-3">
+                Revolutionize the way you design and implement web-based
+                applications
+              </h4>
+              <p>
+                Graphical notation that provides a UML-like notation
+                specifically tailored to the needs of inter-organizational
+                web-based applications. Empower your team, visualize, design,
+                and collaborate effortlessly, bridging the gap between concept
+                and implementation.
+              </p>
             </div>
           </MDBCol>
-        </MDBRow>
+        </div>
       </MDBContainer>
     </PageLayout>
   );
