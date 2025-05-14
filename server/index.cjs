e638c43f24528b04a389b7eee4154183f3b2c99f
JannatulFerdous18 <jannatul.uiu@gmail.com>
2025-05-14 21:42:33 +0200

    Change file extensions

--- server/index.cjs
+++ server/index.cjs
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 // const pg = require("pg");
-const supabase = require("./supabaseClient");
+const supabase = require("./supabaseClient.cjs");
 const bcrypt = require("bcrypt");
 
 dotenv.config();
